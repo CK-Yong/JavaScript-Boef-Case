b89a1d6116e4d264affbb49f398cdc2c9c851f58
CKYong <ckyoung@sogyo.nl>
2017-05-18 13:56:01 +0200

    Written and passed all tests.
    Next step would be visualisation.

--- boef-spec.js
+++ boef-spec.js
@@ -1,3 +1,130 @@
-(function() {
-    // Testen komen hier...
+(function () {
+    describe("The Boef namespace", function () {
+        it("should exist", function () {
+            expect(Boef).toBeDefined();
+        })
+    })
+
+    describe("An emitter", function () {
+        it("should be added", function () {
+            Boef.placeEmitter(52.102403, 5.175269);
+            expect(Boef.emitters().length).toBe(1);
+        })
+    })
+
+    describe("The sensors", function () {
+        it("should be added", function () {
+            Boef.placeSensor(52.102001, 5.173681);
+            Boef.placeSensor(52.101388, 5.176438);
+            Boef.placeSensor(52.101019, 5.175151);
+            expect(Boef.sensors().length).toBe(3);
+        })
+    })
+
+    describe("The distance between sensor and emitter", function () {
+        it("should reset on call", function () {
+            Boef.reset();
+            expect(Boef.sensors().length).toBe(0);
+            expect(Boef.emitters().length).toBe(0);
+        })
+        it("should be about 100 meters distance apart", function () {
+            Boef.reset();
+            Boef.placeEmitter(52.102346, 5.175269);
+            Boef.placeSensor(52.101448, 5.175354);
+            expect(Boef.sensors()[0].distance()).toBeCloseTo(100, 0);
+        });
+        it("should be about 200 meters distance apart", function () {
+            Boef.reset();
+            Boef.placeEmitter(52.102346, 5.175269);
+            Boef.placeSensor(52.100552, 5.175363);
+            expect(Boef.sensors()[0].distance()).toBeCloseTo(200, 0);
+        });
+        it("should be about 300 meters distance apart", function () {
+            Boef.reset();
+            Boef.placeEmitter(52.102346, 5.175269);
+            Boef.placeSensor(52.099647, 5.175377);
+            expect(Boef.sensors()[0].distance()).toBeCloseTo(300, 0);
+        });
+    })
+
+    describe("Detecting of resources", function () {
+        it("should detect 100 meters of normal soil", function () {
+            Boef.reset();
+            Boef.placeEmitter(52.102346, 5.175269);
+            Boef.placeSensor(52.101448, 5.175354);
+            Boef.sensors()[0].pulse(0.0239463601532567);
+            expect(Boef.sensors()[0].amountOfResourcesInMeters()).toBeCloseTo(0.00, 0);
+        });
+
+        it("should detect 100 meters of resources", function () {
+            Boef.reset();
+            Boef.placeEmitter(52.102346, 5.175269);
+            Boef.placeSensor(52.101448, 5.175354);
+            Boef.sensors()[0].pulse(0.0669792364367046);
+            expect(Boef.sensors()[0].amountOfResourcesInMeters()).toBeCloseTo(100.00, 0);
+        });
+
+        it("should detect 50 meters of resources", function () {
+            Boef.reset();
+            Boef.placeEmitter(52.102346, 5.175269);
+            Boef.placeSensor(52.101448, 5.175354);
+            Boef.sensors()[0].pulse(0.0454627982949807);
+            expect(Boef.sensors()[0].amountOfResourcesInMeters()).toBeCloseTo(50.00, 0);
+        });
+
+        it("should detect 20 meters of resources", function () {
+            Boef.reset();
+            Boef.placeEmitter(52.102346, 5.175269);
+            Boef.placeSensor(52.101448, 5.175354);
+            Boef.sensors()[0].pulse(0.0325529354099463);
+            expect(Boef.sensors()[0].amountOfResourcesInMeters()).toBeCloseTo(20.00, 0);
+        });
+
+        it("should detect 83 meters of resources", function () {
+            Boef.reset();
+            Boef.placeEmitter(52.102346, 5.175269);
+            Boef.placeSensor(52.101448, 5.175354);
+            Boef.sensors()[0].pulse(0.0596636474685185);
+            expect(Boef.sensors()[0].amountOfResourcesInMeters()).toBeCloseTo(83.00, 0);
+        })
+    })
+
+    describe("the placing of rows", function(){
+        it("should exist", function(){
+            Boef.reset();
+            Boef.placeEmitter(52.102346, 5.175269);
+            Boef.placeSensors(52.101448,5.175354, 1);
+            expect(Boef.rows().length).toBe(1);
+        });
+
+        it("should contain a sensor", function(){
+            Boef.reset();
+            Boef.placeEmitter(52.102346, 5.175269);
+            Boef.placeSensors(52.101448,5.175354, 1);
+            expect(Boef.rows()[0].length).toBe(1);
+        });
+
+        it("should contain 10 sensors", function(){
+            Boef.reset();
+            Boef.placeEmitter(52.102346, 5.175269);
+            Boef.placeSensors(52.101448,5.175354, 10);
+            expect(Boef.rows()[0].length).toBe(10);
+        });
+
+        it("should contain a sequence of sensors with 100 meters of distance in between", function(){
+            Boef.reset();
+            Boef.placeEmitter(52.102346, 5.175269);
+            Boef.placeSensors(52.101448,5.175354, 10);
+            expect(Boef.rows()[0][0].distance()).toBeCloseTo(100, 0);
+            expect(Boef.rows()[0][1].distance()).toBeCloseTo(200, 0);
+            expect(Boef.rows()[0][2].distance()).toBeCloseTo(300, 0);
+            expect(Boef.rows()[0][3].distance()).toBeCloseTo(400, 0);
+            expect(Boef.rows()[0][4].distance()).toBeCloseTo(500, 0);
+            expect(Boef.rows()[0][5].distance()).toBeCloseTo(600, 0);
+            expect(Boef.rows()[0][6].distance()).toBeCloseTo(700, 0);
+            expect(Boef.rows()[0][7].distance()).toBeCloseTo(800, 0);
+            expect(Boef.rows()[0][8].distance()).toBeCloseTo(900, 0);
+            expect(Boef.rows()[0][9].distance()).toBeCloseTo(1000, 0);
+        });
+    })
 })();
--- boef.js
+++ boef.js
@@ -1,8 +1,97 @@
 // Module Boef
-Boef = (function() {
-    // private code komt hier...
+Boef = (function () {
+    var emitters = [];
+    var sensors = [];
+    var rows = [];
+    var singleRow = [];
+    var radius = 6371000; //radius of Earth in meters
+    var resourceSpeed = 1493;
+    var soilSpeed = 4176;
+    var resourceTime;
+
+    //Applies the haversine function
+    function hav(delta) {
+        return (Math.sin(delta / 2)) * (Math.sin(delta / 2));
+    }
+
+    function toRadians(value) {
+        return value * Math.PI / 180;
+    }
 
     return {
-        // public code komt hier...
+        placeEmitter: function (latitude, longitude) {
+            emitters.push({latitude: latitude, longitude: longitude});
+        },
+        emitters: function () {
+            return emitters;
+        },
+        placeSensor: function (latitude, longitude) {
+            sensors.push({
+                latitude: latitude,
+                longitude: longitude,
+                distance: function () {
+                    var subFormulaA = hav(toRadians(this.latitude) - toRadians(emitters[0].latitude));
+                    var subFormulaB = Math.cos(toRadians(this.latitude)) * Math.cos(toRadians(emitters[0].latitude));
+                    var subFormulaC = hav(toRadians(this.longitude) - toRadians(emitters[0].longitude));
+
+                    return 2 * radius * Math.asin(Math.sqrt(subFormulaA + (subFormulaB * subFormulaC)));
+                },
+                pulse: function (pulseTime) {
+                    var dist = this.distance();
+                    resourceTime = (soilSpeed * pulseTime - dist) / (soilSpeed - resourceSpeed);
+                },
+                amountOfResourcesInMeters: function () {
+                    return resourceTime * resourceSpeed;
+                }
+            });
+        },
+        placeSensors: function (latitude, longitude, number) {
+            var latitudeDelta = latitude - emitters[0].latitude;
+            var longitudeDelta = longitude - emitters[0].longitude;
+
+            if (singleRow.length !== 0) {
+                latitudeDelta = latitude - singleRow[singleRow.length-1].latitude;
+                longitudeDelta = longitude - singleRow[singleRow.length-1].longitude;
+            }
+
+            singleRow.push({
+                latitude: latitude,
+                longitude: longitude,
+                distance: function () {
+                    var subFormulaA = hav(toRadians(this.latitude) - toRadians(emitters[0].latitude));
+                    var subFormulaB = Math.cos(toRadians(this.latitude)) * Math.cos(toRadians(emitters[0].latitude));
+                    var subFormulaC = hav(toRadians(this.longitude) - toRadians(emitters[0].longitude));
+
+                    return 2 * radius * Math.asin(Math.sqrt(subFormulaA + (subFormulaB * subFormulaC)));
+                },
+                pulse: function (pulseTime) {
+                    var dist = this.distance();
+                    resourceTime = (soilSpeed * pulseTime - dist) / (soilSpeed - resourceSpeed);
+                },
+                amountOfResourcesInMeters: function () {
+                    return resourceTime * resourceSpeed;
+                }
+            });
+
+
+            if (number === 1) {
+                rows.push(singleRow);
+                return;
+            }
+
+            return this.placeSensors(latitude + latitudeDelta, longitude + longitudeDelta, number - 1);
+        },
+        sensors: function () {
+            return sensors;
+        },
+        reset: function () {
+            rows = [];
+            singleRow = [];
+            emitters = [];
+            sensors = [];
+        },
+        rows: function () {
+            return rows;
+        }
     };
 })();
